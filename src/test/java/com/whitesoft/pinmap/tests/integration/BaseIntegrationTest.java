1d625f70fafcbb0ef47d32e29cd63bd7c3c06986
Boris Bondarenko <bb@white-soft.ru>
2015-12-27 22:01:19 +1000

    Tests for functionality

--- src/test/java/com/whitesoft/pinmap/tests/integration/BaseIntegrationTest.java
+++ src/test/java/com/whitesoft/pinmap/tests/integration/BaseIntegrationTest.java
@@ -50,8 +50,8 @@ public abstract class BaseIntegrationTest {
         token = null;
     }
 
-    protected <T extends HttpRequest> HttpRequest setAuthHeader(T request) {
+    protected <T extends HttpRequest> T setAuthHeader(T request) {
 
-        return request.header("x-auth-token", token);
+        return (T)request.header("x-auth-token", token);
     }
 }
