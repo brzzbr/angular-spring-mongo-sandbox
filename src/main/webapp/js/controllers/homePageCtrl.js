894a209899b7a5693c352f7d2b6fb9388be6e0d9
Boris Bondarenko <bb@white-soft.ru>
2015-12-27 13:48:13 +1000

    dblclick on map fires an "Add new pin" window

--- src/main/webapp/js/controllers/homePageCtrl.js
+++ src/main/webapp/js/controllers/homePageCtrl.js
@@ -4,9 +4,9 @@
 (function () {
 
     angular.module('pinmap')
-        .controller('homeCtrl', ['authService', 'pinService', 'userService', homeCtrl]);
+        .controller('homeCtrl', ['$scope', 'authService', 'pinService', 'userService', homeCtrl]);
 
-    function homeCtrl(authService, pinService, userService) {
+    function homeCtrl($scope, authService, pinService, userService) {
 
         var thisCtrl = this;
 
@@ -15,7 +15,9 @@
         // adds GoogleMap
         thisCtrl.map = {};
         // pin info window
-        thisCtrl.window = {};
+        thisCtrl.pinInfo = {};
+        // add pin window
+        thisCtrl.addPin = {};
         // my pins
         thisCtrl.myPins = [];
         // pin events (like click) handler
@@ -27,14 +29,34 @@
         initModel();
         function initModel() {
 
+            // instance of gmap
             thisCtrl.map = {
                 center: {latitude: 0, longitude: 0},
                 zoom: 3,
-                pan: 1
+                pan: 1,
+                control: {},
+                options: {
+                    disableDoubleClickZoom: true,
+                },
+                mapEvents: {
+                    dblclick: function (mapModel, eventName, args) {
+
+                        var latLng = args[0].latLng;
+
+                        thisCtrl.addPin.model = {
+                            latitude: latLng.lat(),
+                            longitude: latLng.lng()
+                        };
+                        thisCtrl.addPin.show = true;
+                        // from documentation: scope apply required because
+                        // this event handler is outside of the angular domain
+                        $scope.$evalAsync();
+                    }
+                }
             };
 
-            thisCtrl.window = {
-                marker: {},
+            // instance of pin info window
+            thisCtrl.pinInfo = {
                 show: false,
                 closeClick: function () {
                     this.show = false;
@@ -43,24 +65,36 @@
                 title: ''
             };
 
+            // instance of add pin window
+            thisCtrl.addPin = {
+                show: false,
+                closeClick: function () {
+                    this.show = false
+                },
+                options: {},
+                title: 'Add new pin!'
+            };
+
+            // instance of pin event handler
             thisCtrl.markerEvents = {
                 click: function (marker, eventName, model) {
-                    thisCtrl.window.model = model;
-                    thisCtrl.window.title = model.name;
-                    thisCtrl.window.description = model.description;
-                    thisCtrl.window.userName = model.userName;
-                    thisCtrl.window.created = model.created;
-                    thisCtrl.window.show = true;
+                    thisCtrl.pinInfo.model = model;
+                    thisCtrl.pinInfo.title = model.name;
+                    thisCtrl.pinInfo.description = model.description;
+                    thisCtrl.pinInfo.userName = model.userName;
+                    thisCtrl.pinInfo.created = model.created;
+                    thisCtrl.pinInfo.show = true;
                 }
             };
 
+            // gets current user for userService
             userService.getCurrentUser()
                 .then(function (result) {
 
                     thisCtrl.firstName = result.fullName;
                 });
 
-
+            // gets pins for current user
             pinService.getMyPins()
                 .then(function (response) {
 
